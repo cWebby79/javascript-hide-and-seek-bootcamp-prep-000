d0efbbcc728db8c844a80ea3f9f4a0637af2ad3c
Chris Webster <cgw57@hotmail.com>
2018-07-19 12:56:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  var ele = document.querySelector(selector);
+  return ele;
+}
 
+function nestedTarget() {
+  var ele = document.querySelector('#nested div.target');
+  return ele;
+}
+
+function increaseRankBy(n) {
+  var ele = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ele.length; i++) {
+    ele[i].innerHTML = parseInt(ele[i].innerHTML) + n;
+    }
+}
+
+function deepestChild() {
+  var ele = document.getElementById('grand-node').querySelectorAll('div');
+  return ele[ele.length-1];
+}
